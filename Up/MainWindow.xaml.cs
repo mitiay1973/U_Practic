7b387b066bad3ebee2a6be2b9e5578e1d67ba74f
Dima Sorokin <sorokindima36@gmail.com>
2023-03-03 15:33:47 +0300

    готовый таймер

--- Up/MainWindow.xaml.cs
+++ Up/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Up
 {
@@ -20,10 +21,50 @@ namespace Up
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer _timer;
+
+        public static readonly DependencyProperty TickCounterProperty = DependencyProperty.Register(
+            "TickCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(default(int)));
         public MainWindow()
         {
             InitializeComponent();
             MainFrame.Navigate(new avtoriz(MainFrame));
+            TickCounter = 150;
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMinutes(1d);
+            _timer.Tick += new EventHandler(Timer_Tick);
+            _timer.Start();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        public int TickCounter
+        {
+            get { return (int)GetValue(TickCounterProperty); }
+            set { SetValue(TickCounterProperty, value); }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            if (--TickCounter <= 0)
+            {
+                var timer = (DispatcherTimer)sender;
+                timer.Stop();
+                if (MessageBox.Show("Чтобы закончить работу и закрыть кабинет на кварцевание нажмите да, если хотите продолжить работу на 5 минут нажмите нет", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                {
+                    TickCounter = 5;
+                    _timer = new DispatcherTimer();
+                    _timer.Interval = TimeSpan.FromMinutes(1d);
+                    _timer.Tick += new EventHandler(Timer_Tick);
+                    _timer.Start();
+                }
+                else
+                {
+                    Close();
+                }
+            }
         }
     }
 }
