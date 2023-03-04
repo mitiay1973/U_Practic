8fc1331a367aaf42da33cac808132499bfa8ed64
Dima Sorokin <sorokindima36@gmail.com>
2023-03-04 15:35:48 +0300

    capcha

---
+++ Up/Capcha.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Capcha : Form
+    {
+        
+        private string text = String.Empty;
+        public Capcha()
+        {
+            InitializeComponent();
+            pictureBox1.Image = this.CreateImage(pictureBox1.Width, pictureBox1.Height);
+        }
+    
+        private Bitmap CreateImage(int Width, int Height)
+        {
+            Random rnd = new Random();
+       
+            Bitmap result = new Bitmap(Width, Height);   //Создадим изображение
+        
+            int Xpos = rnd.Next(0, Width - 125);   //Вычислим позицию текста
+            int Ypos = rnd.Next(15, Height - 25);
+         
+            Brush[] colors = { Brushes.Black,   //Добавим различные цвета
+                     Brushes.Red,
+                     Brushes.RoyalBlue,
+                     Brushes.Green };
+       
+            Graphics g = Graphics.FromImage((Image)result);    //Укажем где рисовать
+           
+            g.Clear(Color.Gray); //Пусть фон картинки будет серым
+    
+            text = String.Empty;   //Сгенерируем текст
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+            for (int i = 0; i < 5; ++i)
+                text += ALF[rnd.Next(ALF.Length)];
+       
+            g.DrawString(text,    //Нарисуем сгенирируемый текст
+                         new Font("Arial", 25),
+                         colors[rnd.Next(colors.Length)],
+                         new PointF(Xpos, Ypos));
+
+            //Добавим немного помех
+            /////Линии из углов
+            g.DrawLine(Pens.Black, 
+                       new Point(0, 0),
+                       new Point(Width - 1, Height - 1));
+            g.DrawLine(Pens.Black,
+                       new Point(0, Height - 1),
+                       new Point(Width - 1, 0));
+            ////Белые точки
+            for (int i = 0; i < Width; ++i)
+                for (int j = 0; j < Height; ++j)
+                    if (rnd.Next() % 20 == 0)
+                        result.SetPixel(i, j, Color.White);
+          
+            return result;
+        }
+
+
+        public int pr=0;
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            pictureBox1.Image = this.CreateImage(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == this.text)
+            {
+                MessageBox.Show("Верно!");
+                pr++;
+                this.Close();
+            }       
+            else
+                MessageBox.Show("Ошибка!");
+           
+        }
+    }
+}
--- Up/str/avtoriz.xaml.cs
+++ Up/str/avtoriz.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
+using Practice;
 
 namespace Up
 {
@@ -40,14 +41,8 @@ namespace Up
         {
 
         }
-        public int vx = 0;
-        List<Up.users> Users = new List<Up.users>();
-        List<Up.Workers> worker = new List<Up.Workers>();
-        List<Up.history> history_login = new List<Up.history>{new history()};
-        List<Up.history> historys = new List<Up.history>();
-        private void Entre_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        public void vxod()
         {
-            vx = 0;
             string user = login.Text;
             string pas = password.Password;
             int count = Entities.GetContext().users.Count();
@@ -62,7 +57,7 @@ namespace Up
                 {
                     if (worker[i].password == pas)
                     {
-                        for (int j = count_hh-1; j >=0; j--)
+                        for (int j = count_hh - 1; j >= 0; j--)
                         {
                             if (historys[j].login == user)
                             {
@@ -80,7 +75,7 @@ namespace Up
                                     history_login[0].login = user;
                                     history_login[0].date = DateTime.Now;
                                     history_login[0].ip = Dns.GetHostName();
-                                    if (historys[j].block<DateTime.Now)
+                                    if (historys[j].block < DateTime.Now)
                                     {
                                         history_login[0].block = date.AddHours(2.5);
                                     }
@@ -104,60 +99,87 @@ namespace Up
                     }
                 }
             }
-                for (int i = 0; i < count; i++)
+            for (int i = 0; i < count; i++)
+            {
+                if (Users[i].login == user)
                 {
-                    if (Users[i].login == user)
+                    if (Users[i].password == pas)
                     {
-                        if (Users[i].password == pas)
+                        for (int j = count_hh - 1; j >= 0; j--)
                         {
-                            for (int j = count_hh-1; j >= 0; j--)
+                            if (historys[j].login == user)
                             {
-                                if (historys[j].login == user)
-                                {
-                                DateTime t=DateTime.Now;
-                                if (historys[j].block!=null)
+                                DateTime t = DateTime.Now;
+                                if (historys[j].block != null)
                                 {
                                     t = (DateTime)historys[j].block;
                                     t = t.AddMinutes(30);
                                 }
-                                    if (DateTime.Now < historys[j].block || t <= DateTime.Now)
+                                if (DateTime.Now < historys[j].block || t <= DateTime.Now)
+                                {
+                                    vx = 1;
+                                    int count_h = Entities.GetContext().history.Count();
+                                    history_login[0].id = count_h + 1;
+                                    history_login[0].login = user;
+                                    history_login[0].date = DateTime.Now;
+                                    history_login[0].ip = Dns.GetHostName();
+                                    if (historys[j].block < DateTime.Now)
                                     {
-                                        vx = 1;
-                                        int count_h = Entities.GetContext().history.Count();
-                                        history_login[0].id = count_h + 1;
-                                        history_login[0].login = user;
-                                        history_login[0].date = DateTime.Now;
-                                        history_login[0].ip = Dns.GetHostName();
-                                        if (historys[j].block < DateTime.Now)
-                                        {
-                                            history_login[0].block = date.AddHours(2.5);
-                                        }
-                                        else
-                                        {
-                                            history_login[0].block = historys[j].block;
-                                        }
-                                        Entities.GetContext().history.Add(history_login[0]);
-                                        Entities.GetContext().SaveChanges();
-                                    frame1.Navigate(new glavn(Users[i].login, frame1));
-                                    break;
+                                        history_login[0].block = date.AddHours(2.5);
                                     }
                                     else
                                     {
-                                        MessageBox.Show("Перерыв пол часа");
+                                        history_login[0].block = historys[j].block;
+                                    }
+                                    Entities.GetContext().history.Add(history_login[0]);
+                                    Entities.GetContext().SaveChanges();
+                                    frame1.Navigate(new glavn(Users[i].login, frame1));
+                                    break;
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Перерыв пол часа");
                                     vx = 1;
                                     break;
-                                    }
                                 }
-
-
                             }
+
+
                         }
                     }
                 }
-                if (vx == 0)
+            }
+            if (vx == 0)
+            {
+                MessageBox.Show("Неверный логин или пароль");
+                error++;
+            }
+        }
+        public int vx = 0;
+        List<Up.users> Users = new List<Up.users>();
+        List<Up.Workers> worker = new List<Up.Workers>();
+        List<Up.history> history_login = new List<Up.history>{new history()};
+        List<Up.history> historys = new List<Up.history>();
+        int error=0;
+        private void Entre_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Capcha capcha = new Capcha();
+            if (error > 1)
+            {
+                capcha.ShowDialog();
+                
+                if(capcha.pr>0)
                 {
-                    MessageBox.Show("Неверный логин или пароль");
+                    vx = 0;
+                    vxod();
                 }
+            }
+            else
+            {
+                vx = 0;
+                vxod();           
+            }
+               
             }
         
         private void Reg_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
